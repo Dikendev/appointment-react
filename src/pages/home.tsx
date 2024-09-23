e2a01c9dceb22ca7f6e4f446cda7008cf8627934
Dikendev <diken.dev@gmail.com>
2024-09-22 23:43:16 -0300

    Refactor generateWeekDays and generateTimes functions for improved formatting and display

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,85 +1,133 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  EventHandler,
+  ReactEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { createSwapy } from "swapy";
+import generateTimes from "./hours";
+import { ItemOptions } from "../modal/ItemOptions";
+import { WeekDays } from "./week-days";
 
-const DEFAULT = {
-  "1": "a",
-  "3": "c",
-  "4": "d",
-  "2": null,
+const startTime = "08:00";
+const endTime = "24:00";
+const interval = 30;
+
+const user1 = {
+  name: "DIEGO",
+  appointmentDescription: "Haircut",
+  bookingLength: 4,
+  hours: [
+    {
+      startTime: "08:00",
+      endTime: "09:30",
+    },
+  ],
 };
 
+const user2 = {
+  name: "Jane Doe",
+  appointmentDescription: "Nails",
+  hours: [
+    {
+      startTime: "13:00",
+      endTime: "14:00",
+    },
+  ],
+};
+
+const bookingsByHours = [
+  {
+    day: "Tue : 24",
+    bookings: [
+      {
+        hour: "08:00",
+        user: user1,
+      },
+      {
+        hour: "08:30",
+        user: user1,
+      },
+      {
+        hour: "09:00",
+        user: user1,
+      },
+      {
+        hour: "09:30",
+        user: user1,
+      },
+      {
+        hour: "10:00",
+        user: null,
+      },
+      {
+        hour: "10:30",
+        user: user2,
+      },
+      {
+        hour: "11:00",
+        user: user2,
+      },
+      {
+        hour: "11:30",
+        user: null,
+      },
+      {
+        hour: "12:00",
+        user: null,
+      },
+      {
+        hour: "12:30",
+        user: null,
+      },
+      {
+        hour: "13:00",
+        user: null,
+      },
+      {
+        hour: "13:30",
+        user: null,
+      },
+      {
+        hour: "14:00",
+        user: null,
+      },
+      {
+        hour: "14:30",
+        user: null,
+      },
+      {
+        hour: "15:00",
+        user: null,
+      },
+      {
+        hour: "15:30",
+        user: null,
+      },
+    ],
+  },
+];
+
 export const HomePage = () => {
-  const [selectedBooking, setSelectedBooking] = useState(null);
+  const [selectedBooking, setSelectedBooking] = useState("nada selecionado");
   const [selectedSlot, setSelectedSlot] = useState("");
+  const [rowSpans, setRowSpans] = useState<{ [key: string]: number }>({
+    "12:30": 3,
+  });
 
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-  const appointments = [
-    {
-      name: "Haircut",
-      date: "2021-06-01",
-      time: "10:00",
-    },
-  ];
-
-  const appointments2 = [
-    {
-      name: "Haircut 2",
-      date: "2021-06-01",
-      time: "10:00",
-    },
-  ];
-
-  const appointments3 = [
-    {
-      name: "Haircut 3",
-      date: "2021-06-01",
-      time: "10:00",
-    },
-  ];
-
-  const clients = [
-    {
-      name: "John Doe",
-      age: 25,
-      appointments: appointments,
-    },
-    {
-      name: "Jane Doe",
-      age: 20,
-      appointments: appointments2,
-    },
-    {
-      name: "John Doe 2",
-      age: 30,
-      appointments: appointments3,
-    },
-  ];
-
-  const clients2 = [
-    {
-      name: "John Doe",
-      age: 25,
-      appointments: appointments,
-    },
-    {
-      name: "Jane Doe",
-      age: 20,
-      appointments: appointments2,
-    },
-  ];
-
-  const handleMouseDown = (e: any, index: string) => {
-    // console.log("handleMouseDown", index);
-    console.log("slot", index);
-    setSelectedSlot(index);
+  const handleMouseDown = (e: any) => {
+    console.log("handleMouseDown", e);
+    // setSelectedSlot(index);
   };
 
-  const handleMouseUp = (e: any, index: string) => {
-    // console.log("handleMouseUp", index);
+  const handleMouseUp = (e: any) => {
+    console.log("handleMouseUp", e);
 
-    console.log("slot", index);
-    setSelectedSlot("");
+    // setSelectedSlot("");
   };
 
   const handleOptions = (event: any, data: any) => {
@@ -100,21 +148,88 @@ export const HomePage = () => {
     [selectedBooking]
   );
 
+  const getTheTr = (event) => {
+    console.log("getTheTr", event);
+
+    console.log("getTheTr", event.target.attributes);
+  };
+
   useEffect(() => {
     console.log("useEffect");
     const container = document.querySelector(".container");
     const swapy = createSwapy(container, {
-      animation: "none",
+      animation: "dynamic",
     });
-
     swapy.onSwap(({ data }) => {
       console.log("onSwap", data);
     });
-
     return () => {
       swapy.destroy();
     };
   }, []);
+
+  const generateDayHours = generateTimes(startTime, endTime, interval);
+
+  const weekDays = useMemo(() => WeekDays.generateWeekDays(), []);
+
+  const functionTest = (hour: string, weekDays: string[]) => {
+    const trs = [];
+
+    for (let i = 0; i <= weekDays.length - 1; i++) {
+      const actualDay = weekDays[i];
+      const sunday = i == 0;
+
+      const day = bookingsByHours.find((day) => day.day === actualDay);
+      console.log("day", day);
+
+      if (day) {
+        console.log("day FOUND", day);
+        const rowSpanLength = day.bookings[0].
+        const randomNumber = Math.floor(Math.random() * 100000);
+
+        if (hour === day.bookings[0].hour) {
+          trs.push(
+            <td
+              rowSpan={4}
+              onClick={(event) => getTheTr(event)}
+              onMouseDown={(event) => handleMouseDown(event)}
+              onMouseUp={(event) => handleMouseUp(event)}
+              className={`slot ${randomNumber}`}
+              data-swapy-slot={randomNumber}
+            >
+              <div
+                className={`item ${randomNumber}`}
+                data-swapy-item={randomNumber}
+              >
+                <div className="handle" data-swapy-handle>
+                  {day.bookings[0].user ? day.bookings[0].user.name : "empty"}
+                </div>
+              </div>
+            </td>
+          );
+        } else {
+          const newRandomNumber = Math.floor(Math.random() * 100000);
+          console.log("newRandomNumber", newRandomNumber);
+          trs.push(
+            <td
+              className={`slot ${newRandomNumber} empty`}
+              data-swapy-slot={newRandomNumber}
+            >
+              <div
+                className={`item ${newRandomNumber}`}
+                data-swapy-item={newRandomNumber}
+              >
+                <div>=</div>
+              </div>
+            </td>
+          );
+        }
+
+        console.log("hour", hour);
+      }
+    }
+    return trs;
+  };
 
   return (
     <>
@@ -122,57 +237,21 @@ export const HomePage = () => {
         <thead className="table_header">
           <tr>
             <th>Hora</th>
-            <th>Dia 1</th>
-            <th>Dia 2</th>
+            {weekDays.map((day) => (
+              <th key={day}>{day}</th>
+            ))}
           </tr>
         </thead>
         <tbody className="table_new">
-          {clients.map((client: any, index: any) => (
-            <tr key={client.name}>
-              <td>8:00</td>
-              <td className={`slot ${index}`} data-swapy-slot={index}>
-                <div className={`item ${index}`} data-swapy-item={index}>
-                  <div
-                    className={selectedSlot === client.name ? "selected" : ""}
-                    onMouseDown={(e) => handleMouseDown(e, client.name)}
-                    onMouseUp={(e) => handleMouseUp(e, client.name)}
-                  >
-                    {client.name} - {client.appointments[0].name} -{" "}
-                    <button onClick={(event) => handleOptions(event, client)}>
-                      options
-                    </button>
-                  </div>
-                </div>
-              </td>
-              <td className={`slot ${index + 20}`} data-swapy-slot={index + 20}>
-                <div
-                  className={`item ${index + 20}`}
-                  data-swapy-item={index + 20}
-                >
-                  <div
-                    className={selectedSlot === client.name ? "selected" : ""}
-                    onMouseDown={(e) => handleMouseDown(e, client.name)}
-                    onMouseUp={(e) => handleMouseUp(e, client.name)}
-                  >
-                    {client.name} - {client.appointments[0].name} -{" "}
-                    <button onClick={(event) => handleOptions(event, client)}>
-                      options
-                    </button>
-                  </div>
-                </div>
-              </td>
+          {generateDayHours.map((hour) => (
+            <tr key={hour}>
+              <td>{hour}</td>
+              {functionTest(hour, weekDays)}
             </tr>
           ))}
         </tbody>
       </table>
-      {isModalOpen && (
-        <div>
-          {" "}
-          <span>Usuário:{selectedBooking} </span>
-          <br />
-          <span> X - delete {} O - Edit</span>
-        </div>
-      )}
+      {isModalOpen && <ItemOptions selectedBooking={selectedBooking} />}
     </>
   );
 };
