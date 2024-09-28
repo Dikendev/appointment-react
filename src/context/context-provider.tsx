e61ac1e72889767947d03c7e4d91fefdfb1df10b
Dikendev <diken.dev@gmail.com>
2024-09-27 23:58:40 -0300

    Refactor context provider to include new event modal, hours, and days of the week

--- src/context/context-provider.tsx
+++ src/context/context-provider.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { user1, user2 } from "./mock-data";
 import GlobalContext from "./global-context";
-import { Event } from "../pages/home";
+import generateTimes from "../pages/hours";
+import { WeekDays } from "../pages/week-days";
+import { Event } from "../@types/event";
 
 const initialEvents: Event[] = [
   {
@@ -27,14 +29,28 @@ const initialEvents: Event[] = [
   },
 ];
 
+const START_TIME = "08:00";
+const END_TIME = "24:00";
+const INTERVAL = 30;
+
 const ContextProvider: React.FC<React.PropsWithChildren<object>> = (props) => {
   const [events, setEvents] = useState(initialEvents);
+  const [newEventModal, setNewEventModal] = useState<boolean>(false);
+  const [hours, setHours] = useState<string[]>(() =>
+    generateTimes(START_TIME, END_TIME, INTERVAL)
+  );
+  const daysOfWeek = useMemo(() => WeekDays.generateWeekDays(), []);
 
   return (
     <GlobalContext.Provider
       value={{
         events,
         setEvents: setEvents,
+        newEventModal,
+        setNewEventModal,
+        hours,
+        setHours,
+        daysOfWeek,
       }}
     >
       {props.children}
