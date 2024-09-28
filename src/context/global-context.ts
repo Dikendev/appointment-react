7cfb21531264870f73945110a18d64a9760ce6ae
Dikendev <diken.dev@gmail.com>
2024-09-27 23:58:47 -0300

    Refactor context provider to include new event modal, hours, and days of the week

--- src/context/global-context.ts
+++ src/context/global-context.ts
@@ -1,14 +1,24 @@
 import { createContext, Dispatch, SetStateAction } from "react";
-import { Event } from "../pages/home";
+import { Event } from "../@types/event";
 
 interface GlobalContextType {
   events: Event[];
   setEvents: Dispatch<SetStateAction<Event[]>>;
+  newEventModal: boolean;
+  setNewEventModal: Dispatch<SetStateAction<boolean>>;
+  hours: string[];
+  setHours: Dispatch<SetStateAction<string[]>>;
+  daysOfWeek: string[];
 }
 
 const GlobalContext = createContext<GlobalContextType>({
   events: [],
   setEvents: () => {},
+  newEventModal: false,
+  setNewEventModal: () => {},
+  hours: [],
+  setHours: () => {},
+  daysOfWeek: [],
 });
 
 export default GlobalContext;
