b1436e4d80e26eb44c51e3ff3d551f515a9fff56
Dikendev <diken.dev@gmail.com>
2024-10-01 00:53:40 -0300

    Refactor Header component to include today's date and improve date handling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,72 +1,89 @@
-import { useContext, useState } from "react";
-import GlobalContext from "../../context/global-context";
-
-const MONTH = [
-  "janeiro",
-  "fevereiro",
-  "março",
-  "abril",
-  "maio",
-  "junho",
-  "julho",
-  "agosto",
-  "setembro",
-  "outubro",
-  "novembro",
-  "dezembro",
-];
+import { useCallback, useContext, useEffect, useState } from "react";
+import GlobalContext from "../context/global/global-context";
+import MONTH from "../../constants/month";
+import { DateUtils } from "../../pages/week-days";
 
 const Header = () => {
-  const [month, setMonth] = useState<number>(new Date().getMonth());
-  const [fullYear, setFullYear] = useState<number>(new Date().getFullYear());
-  const { nextWeek, previousWeek } = useContext(GlobalContext);
-  const [monthMessage, setMonthMessage] = useState<string>(MONTH[month]);
+  const { todayWeek, nextWeek, previousWeek } = useContext(GlobalContext);
 
-  // TODO: Está causando bug nessa lógica.
-  const handleOnIncrementMonth = () => {
-    const { lastDayOfWeek, firstDayOfWeek } = nextWeek();
-    const monthInitial = firstDayOfWeek.getMonth();
-    const monthFinal = lastDayOfWeek.getMonth();
+  const [dateInfo, setDateInfo] = useState({
+    month: new Date().getMonth(),
+    fullYear: new Date().getFullYear(),
+    monthMessage: "",
+  });
 
-    if (month === 12) {
-      setMonth(0);
-      setFullYear((prev) => prev + 1);
-      setMonthMessage(MONTH[monthInitial]);
-    } else {
+  const updateDateInfo = useCallback(
+    (targetDate: Date) => {
+      setDateInfo((prev) => ({
+        ...prev,
+        month: targetDate.getMonth(),
+        fullYear: targetDate.getFullYear(),
+        monthMessage: MONTH[targetDate.getMonth()],
+      }));
+    },
+    [setDateInfo]
+  );
+
+  const updateMonthMessages = useCallback(
+    (...targetDate: Date[]) => {
+      setDateInfo((prev) => ({
+        ...prev,
+        monthMessage: DateUtils.shortMonthDescription(
+          targetDate[0],
+          targetDate[1]
+        ),
+      }));
+    },
+    [setDateInfo]
+  );
+
+  const manageDateAndViewMessages = useCallback(
+    (firstDayOfWeek: Date, lastDayOfWeek: Date) => {
+      const monthInitial = firstDayOfWeek.getMonth();
+      const monthFinal = lastDayOfWeek.getMonth();
+
+      updateDateInfo(lastDayOfWeek);
+
       if (monthInitial !== monthFinal) {
-        setMonth((prev) => prev + 1);
-        setMonthMessage(`${MONTH[monthInitial]} - ${MONTH[monthFinal]}`);
-      } else {
-        setMonthMessage(MONTH[monthInitial]);
+        return updateMonthMessages(firstDayOfWeek, lastDayOfWeek);
       }
-    }
+    },
+    [updateDateInfo, updateMonthMessages]
+  );
+
+  const handleWeekChange = (
+    weekChangeFunction: () => { lastDayOfWeek: Date; firstDayOfWeek: Date }
+  ) => {
+    const { lastDayOfWeek, firstDayOfWeek } = weekChangeFunction();
+    manageDateAndViewMessages(firstDayOfWeek, lastDayOfWeek);
   };
 
-  const handleOnDecrementMonth = () => {
-    const { lastDayOfWeek, firstDayOfWeek } = previousWeek();
-    const monthInitial = firstDayOfWeek.getMonth();
-    const monthFinal = lastDayOfWeek.getMonth();
+  useEffect(() => {
+    const { lastDayOfWeek, firstDayOfWeek } = todayWeek();
+    const firstDayOfWeekMonth = firstDayOfWeek.getMonth();
+    const initialMessage =
+      firstDayOfWeekMonth !== lastDayOfWeek.getMonth()
+        ? DateUtils.shortMonthDescription(firstDayOfWeek, lastDayOfWeek)
+        : MONTH[firstDayOfWeekMonth];
 
-    if (month !== 0) {
-      if (monthInitial !== monthFinal) {
-        setMonthMessage(`${MONTH[monthInitial]} - ${MONTH[monthFinal]}`);
-        setMonth((prev) => prev - 1);
-      } else {
-        setMonthMessage(MONTH[monthInitial]);
-      }
-    } else {
-      setMonth(11);
-      setFullYear(fullYear - 1);
-      setMonthMessage(MONTH[monthInitial]);
-    }
-  };
+    setDateInfo((prev) => ({
+      ...prev,
+      monthMessage: initialMessage,
+    }));
+  }, []);
 
   return (
     <header className="flex flex-row gap-2">
       <div className="flex flex-row">
         <button
+          className="bg-white hover:bg-gray-100 border-[1px] border-gray-500 border-solid py-2 self-center px-3 flex hover:border-gray-500 focus:outline-0 active:bg-gray-300"
+          onClick={() => handleWeekChange(todayWeek)}
+        >
+          <span className="text-black">Today</span>
+        </button>
+        <button
           className="bg-white hover:bg-gray-100 border-none rounded-full py-3 px-3 flex focus:bg-gray-200 focus:outline-0"
-          onClick={() => handleOnDecrementMonth()}
+          onClick={() => handleWeekChange(previousWeek)}
         >
           <span className="material-symbols-outlined text-gray-500">
             chevron_left
@@ -74,13 +91,15 @@ const Header = () => {
         </button>
         <button
           className="bg-white hover:bg-gray-100 border-none rounded-full py-3 px-3 flex text-gray-500 target:border-none focus:bg-gray-200 focus:outline-0"
-          onClick={() => handleOnIncrementMonth()}
+          onClick={() => handleWeekChange(nextWeek)}
         >
           <span className="material-symbols-outlined">chevron_right</span>
         </button>
       </div>
-      <div className="w-56 content-center">
-        {monthMessage} - {fullYear}
+      <div className="w-40 content-center">
+        <span className=" text-gray-800 font-bold">
+          {dateInfo.monthMessage} {dateInfo.fullYear}
+        </span>
       </div>
     </header>
   );
