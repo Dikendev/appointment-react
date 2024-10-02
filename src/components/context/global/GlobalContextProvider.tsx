eb9aa7181c498d0c5a53211b3d1fb389e1081759
Dikendev <diken.dev@gmail.com>
2024-10-01 22:53:28 -0300

    Refactor GlobalContextProvider to include booking view type selection and handle day change actions

--- src/components/context/global/GlobalContextProvider.tsx
+++ src/components/context/global/GlobalContextProvider.tsx
@@ -4,12 +4,69 @@ import GlobalContext from "./global-context";
 import { Bookings } from "../../../@types/booking";
 import generateTimes, { Times } from "../../../pages/hours";
 import { DateUtils, WeekDaysList } from "../../../pages/week-days";
+import BOOKING_VIEW_TYPE from "../../../constants/booking-view";
 
 const today = new Date();
 
 const COLORS = ["#03fcdf", "#03a5fc", "#fc036b"];
 
 const initialEvents: Bookings = [
+  {
+    id: "1",
+    startAt: new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate(),
+      8,
+      0o0
+    ),
+    finishAt: new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate(),
+      9,
+      30
+    ),
+    client: user1,
+    procedure: {
+      id: "1",
+      color: COLORS[0],
+      name: "Haircut",
+      price: 50,
+      requiredTimeMin: 90,
+    },
+    total: 50,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: "1",
+    startAt: new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate(),
+      11,
+      30
+    ),
+    finishAt: new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate(),
+      12,
+      30
+    ),
+    client: user1,
+    procedure: {
+      id: "1",
+      color: COLORS[0],
+      name: "Haircut",
+      price: 50,
+      requiredTimeMin: 90,
+    },
+    total: 50,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
   {
     id: "1",
     startAt: new Date(
@@ -105,6 +162,8 @@ export interface NextAndPreviousWeek {
   lastDayOfWeek: Date;
 }
 
+export type ActionDay = "today" | "previous" | "next";
+
 const GlobalContextProvider: React.FC<React.PropsWithChildren<object>> = ({
   children,
 }) => {
@@ -113,6 +172,8 @@ const GlobalContextProvider: React.FC<React.PropsWithChildren<object>> = ({
   const [hours] = useState<Times>(() =>
     generateTimes(START_TIME, END_TIME, INTERVAL)
   );
+
+  const [bookingType, setBookingType] = useState<string>(BOOKING_VIEW_TYPE[1]);
 
   const {
     week: initialWeek,
@@ -123,6 +184,11 @@ const GlobalContextProvider: React.FC<React.PropsWithChildren<object>> = ({
   const firstDayOfWeekRef = useRef(firstDayOfWeek);
   const lastDayOfWeekRef = useRef(lastDayOfWeek);
   const [week, setWeek] = useState<WeekDaysList>(initialWeek);
+  const setTodayDay = (date: Date) => {
+    const todaysDayDate = DateUtils.generateDays(date, 0);
+    setWeek(() => todaysDayDate);
+    return todaysDayDate;
+  };
 
   const todayWeek = (): NextAndPreviousWeek => {
     const { week, firstDayOfWeek, lastDayOfWeek } =
@@ -140,7 +206,7 @@ const GlobalContextProvider: React.FC<React.PropsWithChildren<object>> = ({
   };
 
   const nextWeek = (): NextAndPreviousWeek => {
-    const adding = DateUtils.addDayOrMinus(lastDayOfWeekRef.current, true);
+    const adding = DateUtils.addDay(lastDayOfWeekRef.current, 1);
     const result = DateUtils.generateWeekDays(adding);
 
     setWeek(result.week);
@@ -154,8 +220,33 @@ const GlobalContextProvider: React.FC<React.PropsWithChildren<object>> = ({
     };
   };
 
+  const handleDayChange = (actionDay: ActionDay): WeekDaysList | undefined => {
+    switch (actionDay) {
+      case "next": {
+        return setDays(1);
+      }
+      case "previous": {
+        return setDays(-1);
+      }
+    }
+  };
+
+  const setDays = (days: number): WeekDaysList | undefined => {
+    const key = week.keys().next().value;
+
+    if (!key) return;
+
+    const actualDay = week.get(key);
+    if (!actualDay) return;
+
+    const day = DateUtils.generateDays(actualDay, days);
+    setWeek(day);
+
+    return day;
+  };
+
   const previousWeek = (): NextAndPreviousWeek => {
-    const minus = DateUtils.addDayOrMinus(firstDayOfWeekRef.current, false);
+    const minus = DateUtils.addDay(firstDayOfWeekRef.current, -1);
     const result = DateUtils.generateWeekDays(minus);
 
     setWeek(result.week);
@@ -182,6 +273,10 @@ const GlobalContextProvider: React.FC<React.PropsWithChildren<object>> = ({
         todayWeek,
         nextWeek,
         previousWeek,
+        bookingType,
+        setBookingType,
+        setTodayDay,
+        handleDayChange,
       }}
     >
       {children}
