d31ad7675a0c7e09cd687501e201b6d3ac2c0c5a
Dikendev <diken.dev@gmail.com>
2024-10-07 21:56:59 -0300

    Refactor view types

--- src/components/ViewTypes.tsx
+++ src/components/ViewTypes.tsx
@@ -1,9 +1,7 @@
 import { useContext, useState } from "react";
-import FormAddNewEvent from "../pages/forms/FormAddNewEvent";
 import Header from "./header-calendar/Header";
 import BOOKING_VIEW_TYPE from "../constants/booking-view";
 import DaysWeek from "./header-calendar/DaysOfWeek";
-import AppointmentFilterType from "./AppointmentFilterType";
 import GlobalContext from "../context/global/global-context";
 import EventContextProvider from "../context/BookingContextProvider";
 import CalendarView from "./CalendarView";
