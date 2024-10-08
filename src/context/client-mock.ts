9a38ebf161563a5f067526a6750987e713ea4044
Dikendev <diken.dev@gmail.com>
2024-10-07 22:02:21 -0300

    Add client mock

---
+++ src/context/client-mock.ts
@@ -0,0 +1,20 @@
+import { Client } from "../@types/booking";
+
+export const ClientMock: Client[] = [
+  {
+    id: "1",
+    name: "Diego cliente",
+  },
+  {
+    id: "2",
+    name: "David cliente",
+  },
+  {
+    id: "3",
+    name: "Kennedy cliente",
+  },
+  {
+    id: "4",
+    name: "Cris cliente",
+  },
+];
