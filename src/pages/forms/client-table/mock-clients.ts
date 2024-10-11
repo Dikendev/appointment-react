41abc4005ebff6e86914c53c99e1e75db5763dee
Dikendev <diken.dev@gmail.com>
2024-10-10 23:41:47 -0300

    Add mock clients

---
+++ src/pages/forms/client-table/mock-clients.ts
@@ -0,0 +1,28 @@
+import { Client } from "../../../@types/client";
+
+export const mockClientData: Client[] = [
+  {
+    id: "m5gr84i9",
+    name: "diego",
+    phoneNumber: "999,0099,99",
+    birthDayDate: "12/23/12",
+  },
+  {
+    id: "3u1reuv4",
+    name: "cristina",
+    phoneNumber: "99,0099,99",
+    birthDayDate: "10/23/12",
+  },
+  {
+    id: "derv1ws0",
+    name: "david",
+    phoneNumber: "999,1,99",
+    birthDayDate: "12/23/32",
+  },
+  {
+    id: "5kma53ae",
+    name: "obama",
+    phoneNumber: "999,339,99",
+    birthDayDate: "12/23/42",
+  },
+];
