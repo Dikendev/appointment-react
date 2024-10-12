aebf5638812376e3532cca76571540383da34452
Dikendev <diken.dev@gmail.com>
2024-10-12 20:57:39 -0300

    Feature login context

---
+++ src/context/global/login/login-context.ts
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+import { LoginDto } from "../../../services/sign-in";
+
+interface LoginContextType {
+  saveEmail: (email: string) => void;
+  getEmail: () => void;
+  logOut: () => void;
+  login: (loginDto: LoginDto) => void;
+  email: string | null;
+}
+
+const LoginContext = createContext<LoginContextType>({
+  getEmail: () => {},
+  saveEmail: () => {},
+  logOut: () => {},
+  email: null,
+  login: () => {},
+});
+
+export default LoginContext;
