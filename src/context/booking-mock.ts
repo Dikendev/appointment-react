ea5203c3cc9cfc8b0901924fed6ab001f1ebef6f
Dikendev <diken.dev@gmail.com>
2024-10-22 23:53:01 -0300

    Removing unused files

--- src/context/booking-mock.ts
+++
@@ -1,8 +0,0 @@
-import { ProcedureModal } from "../@types/booking";
-
-export const procedureMock: ProcedureModal[] = [
-  { id: "1", name: "Corte", price: 30, color: "#000" },
-  { id: "2", name: "Coloração", price: 50, color: "#f00" },
-  { id: "3", name: "Escova", price: 20, color: "#0f0" },
-  { id: "4", name: "Sobrancelha", price: 30, color: "#00f" },
-];
