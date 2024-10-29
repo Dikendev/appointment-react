a90e045e58588b51415fa8a656296fe6ada04ce2
Dikendev <diken.dev@gmail.com>
2024-10-29 02:07:14 -0300

    Feature add booking response to week view props

--- src/@types/week-view-props.ts
+++ src/@types/week-view-props.ts
@@ -1,5 +1,7 @@
 import { WeekDaysList } from "../utils/date-utils";
+import { BookingsResponse } from "./booking";
 
 export interface WeekViewProps {
   daysOfWeek: WeekDaysList;
+  bookings: BookingsResponse;
 }
