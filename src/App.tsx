ae1badccd43456bd5f8723be230637c033756636
Dikendev <diken.dev@gmail.com>
2024-10-29 22:49:44 -0300

    Feature add new drag and drop system

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,18 @@ import Header from "./components/header/Header";
 import GlobalContextProvider from "./context/global/GlobalContextProvider";
 import LoginContextProvider from "./context/global/login/LoginContextProvider";
 import { Toaster } from "./components/toast/Toaster";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 
 const App = () => {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <GlobalContextProvider>
-        <LoginContextProvider>
-          <Header />
-          <main>
-            <Outlet />
-          </main>
-          <Toaster />
-        </LoginContextProvider>
-      </GlobalContextProvider>
-    </DndProvider>
+    <GlobalContextProvider>
+      <LoginContextProvider>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Toaster />
+      </LoginContextProvider>
+    </GlobalContextProvider>
   );
 };
 export default App;
