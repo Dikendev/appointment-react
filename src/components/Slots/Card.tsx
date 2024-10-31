25a756932fd8677b4060508122c95bd1aeddac74
Dikendev <diken.dev@gmail.com>
2024-10-30 23:30:47 -0300

    Refactor fix, bug not able to drag more than 1 time

--- src/components/Slots/Card.tsx
+++ src/components/Slots/Card.tsx
@@ -2,6 +2,7 @@ import { CSSProperties } from "react";
 import { Booking } from "../../@types/booking";
 import BookingCard from "../booking-card/BookingCard";
 import { useDraggable } from "@dnd-kit/core";
+import { GripHorizontal } from "lucide-react";
 
 interface CardProps {
   heightStyle: CSSProperties;
@@ -16,6 +17,7 @@ const Card = ({ heightStyle, booking, day, hour, hoursTime }: CardProps) => {
     id: booking.id,
     data: { booking },
   });
+
   const style = transform
     ? {
         transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
@@ -27,9 +29,16 @@ const Card = ({ heightStyle, booking, day, hour, hoursTime }: CardProps) => {
       ref={setNodeRef}
       className="bg-black text-white w-[90%] absolute z-10 border rounded-sm shadow-[5px_5px_8px_2px_rgba(0,0,0,0.3)] overflow-hidden"
       style={{ ...style, ...heightStyle }}
-      {...listeners}
       {...attributes}
     >
+      <div className="flex flex-row justify-end w-full h-[2rem] bg-blue-500">
+        <div
+          className="flex flex-row justify-center items-center w-12 cursor-move"
+          {...listeners}
+        >
+          <GripHorizontal />
+        </div>
+      </div>
       <BookingCard
         booking={booking}
         dateDataStrings={{ day, hour }}
