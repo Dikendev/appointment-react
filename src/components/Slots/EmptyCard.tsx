235fd67f04a5cd29c016f7c94bfb415968bd481b
Dikendev <diken.dev@gmail.com>
2024-11-01 01:04:16 -0300

    Refactor empty card to use only the drop ref as prop

--- src/components/Slots/EmptyCard.tsx
+++ src/components/Slots/EmptyCard.tsx
@@ -1,86 +1,54 @@
-import { useDroppable } from "@dnd-kit/core";
 import { CSSProperties, forwardRef } from "react";
-import { DateUtils } from "../../utils/date-utils";
 
 interface EmptyCardProps {
+  full: {
+    key: string;
+    style: CSSProperties;
+    ref: (element: HTMLElement | null) => void;
+  };
+  half: {
+    key: string;
+    style: CSSProperties;
+    ref: (element: HTMLElement | null) => void;
+  };
   disabledCss: string;
-  dayHour: {
-    day: string;
-    hour: string;
-  };
-  lunchTimeBlock: {
-    startAt: string;
-    finishAt: string;
-  };
   handleTimeClicked: (timeType: "half" | "full") => void;
+  children?: React.ReactNode;
 }
 
 const EmptyCard = forwardRef(
   ({
+    full: { key: fullKey, style: fullStyle, ref: fullHouRef },
+    half: { key: halfKey, style: styleHalf, ref: halfHourRef },
     disabledCss,
-    dayHour: { day, hour },
-    lunchTimeBlock: { startAt, finishAt },
     handleTimeClicked,
+    children,
   }: EmptyCardProps) => {
-    const newDateKey = (date: string, hour: string) => {
-      const newDate = new Date(date);
-      newDate.setHours(Number(hour.split(":")[0]));
-      newDate.setMinutes(Number(hour.split(":")[1]));
-      return newDate.toISOString();
-    };
-
-    const timeWithAddedMinutes = DateUtils.addMinuteToHour(hour, 30);
-
-    const isTimeLunch = (hour: string) => {
-      if (hour === startAt || hour === finishAt) {
-        disabledCss = "bg-gray-200 border-none cursor-not-allowed";
-        return true;
-      }
-      return false;
-    };
-
-    const { isOver, setNodeRef } = useDroppable({
-      id: `${newDateKey(day, hour)}`,
-      disabled: isTimeLunch(hour),
-    });
-
-    const style: CSSProperties = {
-      backgroundColor: isOver ? "green" : "",
-    };
-
-    const { isOver: isOverHalf, setNodeRef: setNodeRefHalf } = useDroppable({
-      id: `${newDateKey(day, timeWithAddedMinutes)}`,
-      disabled: isTimeLunch(timeWithAddedMinutes),
-    });
-
-    const styleHalf: CSSProperties = {
-      backgroundColor: isOverHalf ? "green" : "",
-    };
-
     return (
       <>
         <div
-          ref={setNodeRef}
-          style={style}
-          key={`${newDateKey(day, hour)}`}
+          ref={fullHouRef}
+          style={fullStyle}
+          key={fullKey}
           className={`w-full h-[3rem] relative border-b border-gray-200 ${disabledCss}`}
           onClick={() => handleTimeClicked("full")}
         >
-          <div className="w-[8rem]"></div>
+          {children ? children : <div className="w-[8rem]"></div>}
         </div>
 
         <div
-          ref={setNodeRefHalf}
+          ref={halfHourRef}
           style={styleHalf}
-          key={`${newDateKey(day, timeWithAddedMinutes)}`}
+          key={halfKey}
           className={`w-full h-[3rem] relative ${disabledCss}`}
           onClick={() => handleTimeClicked("half")}
         >
-          <div className="w-[8rem]"></div>
+          {children ? children : <div className="w-[8rem]"></div>}
         </div>
       </>
     );
   }
 );
 
+EmptyCard.displayName = "EmptyCard";
 export default EmptyCard;
