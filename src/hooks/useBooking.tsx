fd307a0ded40a2d220238e37ab03d8b79f106016
Dikendev <diken.dev@gmail.com>
2024-10-31 23:55:31 -0300

    Refactor booking hook

--- src/hooks/useBooking.tsx
+++ src/hooks/useBooking.tsx
@@ -2,33 +2,8 @@ import { useContext } from "react";
 import BookingContext from "../context/booking/booking-context";
 
 const useBooking = () => {
-  const {
-    selectedDate,
-    setSelectedDate,
-    selectedHour,
-    setSelectedHour,
-    openNewBookingModal,
-    procedures,
-    setProcedures,
-    availableHours,
-    setAvailableHours,
-    eventModal,
-    closeEventModal,
-  } = useContext(BookingContext);
-
-  return {
-    selectedDate,
-    setSelectedDate,
-    selectedHour,
-    setSelectedHour,
-    openNewBookingModal,
-    procedures,
-    setProcedures,
-    availableHours,
-    setAvailableHours,
-    eventModal,
-    closeEventModal,
-  };
+  const bookingContext = useContext(BookingContext);
+  return { ...bookingContext };
 };
 
 export default useBooking;
